5418f3dee49808ab366f474e11a1b63a90747809
Madhu B Yallaraddi Madhu <Madhu-B-Yallaraddi.Madhu@dxc.com>
2019-05-17 12:41:47 +0530

    CaseStudySBA Db created

---
+++ CaseStudySBA/CasestudyDb.sql
@@ -0,0 +1,33 @@
+﻿create database CaseStudySBA
+
+use CaseStudySBA
+
+
+drop table Workout_Category
+
+create table Workout_Category
+(
+category_id int identity primary key,
+category_name varchar(64)
+)
+ 
+create table Workout_Collection
+(
+workout_title varchar(128),
+workout_note varchar(256),
+calories_burn_per_min float,
+category_id int  foreign key references Workout_Category(category_id),
+workout_id int identity primary key
+)
+
+
+create table Workout_Active
+(
+workout_id int foreign key references Workout_Collection(workout_id),
+start_time time,
+starts_date date,
+end_time time,
+end_date date,
+comment varchar(64),
+statuss bit
+)
